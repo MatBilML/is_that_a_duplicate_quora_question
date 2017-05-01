803385d98fe685bb580056d25a791096f2ae2b6d
Pandian <pandian4mail@gmail.com>
2017-04-30 21:04:45 -0500

    Modifying condor submit script to use different trigger file names

--- process_condor.sh
+++ process_condor.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters; Please provide run_dir and cpu/gpu as the parameter;"
+if [ "$#" -lt 2 ]; then
+    echo "Illegal number of parameters; Please provide run_dir and cpu/gpu and optionally trigger.sh file name as the parameters;"
     exit 1
+fi
+
+trigger_file='trigger.sh'
+if [ "$#" -eq 3 ]; then
+    trigger_file=$3
 fi
 
 run_dir=$1
@@ -11,6 +16,8 @@ mkdir -p $run_dir
 
 condor_file=$run_dir'/condor.script'
 cat condor."$pu"_script_head > $condor_file
+
+echo 'arguments = '$trigger_file >> $condor_file
 echo 'Log = '$run_dir'/log.log' >> $condor_file
 echo 'Output = '$run_dir'/output.log' >> $condor_file
 echo 'Error  = '$run_dir'/error.log' >> $condor_file
